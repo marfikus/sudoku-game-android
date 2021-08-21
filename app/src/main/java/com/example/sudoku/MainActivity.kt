a77786fe4d34be7b8d299c853711ed1e10faff22
Alexey Zaytsev <alex.rv9rh@gmail.com>
2021-08-21 16:44:44 +0500

    Add buttons_panel, all ceils

--- app/src/main/java/com/example/sudoku/MainActivity.kt
+++ app/src/main/java/com/example/sudoku/MainActivity.kt
@@ -23,18 +23,18 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        ceils.add(
+        ceils.addAll(
             listOf(
-                ceil_00,
-                ceil_01,
-                ceil_02,
-                ceil_03,
-                ceil_04,
-                ceil_05,
-                ceil_06,
-                ceil_07,
-                ceil_08,
-            ),
+                listOf(ceil_00, ceil_01, ceil_02, ceil_03, ceil_04, ceil_05, ceil_06, ceil_07, ceil_08),
+                listOf(ceil_10, ceil_11, ceil_12, ceil_13, ceil_14, ceil_15, ceil_16, ceil_17, ceil_18),                
+                listOf(ceil_20, ceil_21, ceil_22, ceil_23, ceil_24, ceil_25, ceil_26, ceil_27, ceil_28),                
+                listOf(ceil_30, ceil_31, ceil_32, ceil_33, ceil_34, ceil_35, ceil_36, ceil_37, ceil_38),                
+                listOf(ceil_40, ceil_41, ceil_42, ceil_43, ceil_44, ceil_45, ceil_46, ceil_47, ceil_48),                
+                listOf(ceil_50, ceil_51, ceil_52, ceil_53, ceil_54, ceil_55, ceil_56, ceil_57, ceil_58),                
+                listOf(ceil_60, ceil_61, ceil_62, ceil_63, ceil_64, ceil_65, ceil_66, ceil_67, ceil_68),                
+                listOf(ceil_70, ceil_71, ceil_72, ceil_73, ceil_74, ceil_75, ceil_76, ceil_77, ceil_78),                
+                listOf(ceil_80, ceil_81, ceil_82, ceil_83, ceil_84, ceil_85, ceil_86, ceil_87, ceil_88)                
+            )
         )
 
         fillGameField()
@@ -79,8 +79,9 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun ceilClicked(view: View) {
-//        Toast.makeText(applicationContext, "${(view.tag as Pair<*, *>).second}", Toast.LENGTH_SHORT).show()
+        Toast.makeText(applicationContext, "${(view.tag as Pair<*, *>).second}", Toast.LENGTH_SHORT).show()
         view.setBackgroundColor(resources.getColor(R.color.black))
+        buttons_panel.visibility = View.VISIBLE
     }
 
     private fun createGameFieldViews() {
@@ -110,7 +111,7 @@ class MainActivity : AppCompatActivity() {
                 linLayoutHor.addView(button)
             }
 
-            linLayoutVer.addView(linLayoutHor)
+            ceils_field.addView(linLayoutHor)
         }
     }
 
