8f1fda79b09a43dd19b9c05e3ec645ccfb775e57
Alexey Zaytsev <alex.rv9rh@gmail.com>
2021-08-21 15:48:47 +0500

    Change layout to buttons

--- app/src/main/java/com/example/sudoku/MainActivity.kt
+++ app/src/main/java/com/example/sudoku/MainActivity.kt
@@ -5,8 +5,11 @@ import android.os.Bundle
 import android.text.InputFilter
 import android.text.InputType
 import android.util.Log
+import android.view.View
+import android.widget.Button
 import android.widget.EditText
 import android.widget.LinearLayout
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 
@@ -14,15 +17,31 @@ class MainActivity : AppCompatActivity() {
 
     private val LOG_TAG = "LOG_TAG"
     private val gameField = mutableListOf<List<Int>>()
+    private val ceils = mutableListOf<List<Button>>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        ceils.add(
+            listOf(
+                ceil_00,
+                ceil_01,
+                ceil_02,
+                ceil_03,
+                ceil_04,
+                ceil_05,
+                ceil_06,
+                ceil_07,
+                ceil_08,
+            ),
+        )
+
         fillGameField()
         debugPrintGameField()
 
-        createGameFieldViews()
+//        createGameFieldViews()
+        initCeils()
     }
 
     private fun fillGameField() {
@@ -46,6 +65,24 @@ class MainActivity : AppCompatActivity() {
         Log.d(LOG_TAG, result)
     }
 
+    private fun initCeils() {
+
+        for (i in ceils.indices) {
+            for (j in ceils[i].indices) {
+                ceils[i][j].text = gameField[i][j].toString()
+                ceils[i][j].tag = Pair(i, j)
+                ceils[i][j].setOnClickListener {
+                    ceilClicked(it)
+                }
+            }
+        }
+    }
+
+    private fun ceilClicked(view: View) {
+//        Toast.makeText(applicationContext, "${(view.tag as Pair<*, *>).second}", Toast.LENGTH_SHORT).show()
+        view.setBackgroundColor(resources.getColor(R.color.black))
+    }
+
     private fun createGameFieldViews() {
 
         for (i in 0..8) {
@@ -58,22 +95,23 @@ class MainActivity : AppCompatActivity() {
             linLayoutHor.layoutParams = linLayoutHorPar
 
             for (j in 0..8) {
-                val editText = EditText(this)
+                val button = Button(this)
                 var params = LinearLayout.LayoutParams(
                     LinearLayout.LayoutParams.WRAP_CONTENT,
                     LinearLayout.LayoutParams.WRAP_CONTENT
                 )
 //                params.setMargins(10, 10, 0, 0)
 
-                editText.layoutParams = params
-                editText.inputType = InputType.TYPE_CLASS_NUMBER
-                editText.filters = arrayOf(InputFilter.LengthFilter(1))
-                editText.setText(gameField[i][j].toString())
+                button.layoutParams = params
+//                editText.inputType = InputType.TYPE_CLASS_NUMBER
+//                editText.filters = arrayOf(InputFilter.LengthFilter(1))
+                button.text = gameField[i][j].toString()
 
-                linLayoutHor.addView(editText)
+                linLayoutHor.addView(button)
             }
 
             linLayoutVer.addView(linLayoutHor)
         }
     }
+
 }
